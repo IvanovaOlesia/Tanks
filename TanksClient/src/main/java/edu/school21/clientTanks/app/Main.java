418cdb10e7520772d4466c6f238cc46dad596d1d
pallaaxe <ovanovala@yandex.ru>
2024-10-13 19:41:11 +0700

    main

--- TanksClient/src/main/java/edu/school21/clientTanks/app/Main.java
+++ TanksClient/src/main/java/edu/school21/clientTanks/app/Main.java
@@ -1,8 +1,11 @@
 package edu.school21.clientTanks.app;
+import edu.school21.clientTanks.controller.Controller;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.InputStream;
+import java.net.URL;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -10,9 +13,15 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Tanks");
-        stage.setWidth(800);
-        stage.setHeight(600);
-        InputStream iconStream = getClass().getClassLoader().getResourceAsStream("icon.png");
+        URL fxmlUrl = getClass().getResource("/view.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlUrl);
+        Scene scene = new Scene(fxmlLoader.load(),1024, 1024);
+        Controller controller = fxmlLoader.getController();
+        scene.setOnKeyPressed(event -> {
+            controller.moveImageLeft(event.getCode());
+        });
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
