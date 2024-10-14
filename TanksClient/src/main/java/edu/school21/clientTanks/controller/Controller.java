78f5f5cc435a36be0d275f2b17f66846b9785f11
pallaaxe <ovanovala@yandex.ru>
2024-10-14 18:24:59 +0700

    added the enemy field to the controller

--- TanksClient/src/main/java/edu/school21/clientTanks/controller/Controller.java
+++ TanksClient/src/main/java/edu/school21/clientTanks/controller/Controller.java
@@ -1,25 +1,31 @@
 package edu.school21.clientTanks.controller;
 
+import edu.school21.clientTanks.dataGame.PlayerData;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 
 public class Controller {
     @FXML
     private ImageView player;
-
     @FXML
-    private ImageView field;
+    private ImageView enemy;
+    private Scene scene;
+
+    public Controller() {
+    }
+
+    public Controller(Scene scene) {
+        this.scene = scene;
+    }
+
     @FXML
     public void initialize() {
 
     }
-    public  void moveImageLeft( KeyCode keyCode) {
-        if (keyCode == KeyCode.LEFT) {
-            player.setTranslateX(player.getTranslateX() - 10);
-        } else if (keyCode == KeyCode.RIGHT) {
-            player.setTranslateX(player.getTranslateX() + 10);
-        }
-
+    public void moveTank(PlayerData playerData) {
+        player.setTranslateX(playerData.getLayoutPlayerX());
+        enemy.setTranslateX(playerData.getLayoutEnemyX());
     }
 }
--- TanksClient/src/main/java/module-info.java
+++ TanksClient/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module edu.school21.clientTanks {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.dlsc.formsfx;
+    requires com.google.gson;
 
     opens edu.school21.clientTanks.app to javafx.fxml;
     exports edu.school21.clientTanks.app ;
